9d60555fbd5f19c2964e742961edbc2c8072add6
gagan007gowda <gagan007gowda@gmail.com>
2020-04-08 10:22:54 +0530

    First Commit

---
+++ DataStructure/src/LinkedList.java
@@ -0,0 +1,11 @@
+import java.util.Iterator;
+
+public class LinkedList<E> implements Iterable<E>{
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
