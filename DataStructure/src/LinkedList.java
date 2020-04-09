c54747446edbd5c3279f1cfafe11001b4d1e4d1a
gagan007gowda <gagan007gowda@gmail.com>
2020-04-09 12:59:15 +0530

    Linked List

--- DataStructure/src/LinkedList.java
+++ DataStructure/src/LinkedList.java
@@ -1,11 +1,32 @@
-import java.util.Iterator;
-
-public class LinkedList<E> implements Iterable<E>{
-
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+	
+public class LinkedList{
+	Node head;
+	//Creating a node that has data and the referencec to the next data
+	static class Node{
+		int data;
+		Node next;
+		Node(int data){
+			this.data=data;
+			next=null;
+		}
 	}
-
+	
+	public void printNodes() {
+		Node node=head;
+		while(node!=null) {
+			System.out.println(node.data);
+			node=node.next;
+		}
+	}
+	
+	public static void main(String args[]) {
+		LinkedList llist=new LinkedList();
+		llist.head=new Node(1);
+		Node second=new Node(2);
+		Node third= new Node(3);
+		llist.head.next=second;
+		second.next=third;
+		third.next=null;
+		llist.printNodes();
+	}
 }
