c8f38083d56ffa1991aeb3b68ed1fce9ba445c55
gagan007gowda <gagan007gowda@gmail.com>
2020-04-13 11:04:01 +0530

    Insertion on both ends

--- DataStructure/src/LinkedList.java
+++ DataStructure/src/LinkedList.java
@@ -18,6 +18,27 @@ public class LinkedList{
 			node=node.next;
 		}
 	}
+	//Adding a node at the begining of the List
+	void insertBegining(int newdata) {
+		Node newNode=new Node(newdata);
+		newNode.next=head;
+		head=newNode;
+	}
+	
+	void insertEnd(int newdata) {
+		Node newNode=new Node(newdata);
+		newNode.next=null;
+		if(head == null) {
+			head=newNode;
+			return;
+		}
+		Node last=head;
+		while(last.next!=null) {
+			last=last.next;
+		}
+		last.next=newNode;
+		return;
+	}
 	
 	public static void main(String args[]) {
 		LinkedList llist=new LinkedList();
